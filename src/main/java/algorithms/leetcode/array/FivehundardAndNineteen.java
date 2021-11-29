f29b1576b3c98449d7837cb36592fb32ba906105
Chanter-Machine <9924000463@qq.com>
2021-11-29 23:37:05 +0800

    changed class name

--- src/main/java/algorithms/leetcode/array/FivehundardAndNineteen.java
+++ src/main/java/algorithms/leetcode/array/FivehundardAndNineteen.java
@@ -3,16 +3,16 @@ package algorithms.leetcode.array;
 import java.util.HashMap;
 import java.util.Random;
 
-public class FivehundardAndFiftyNine {
+public class FivehundardAndNineteen {
     public static void main(String[] args) {
-        FivehundardAndFiftyNine fivehundardAndFiftyNine = new FivehundardAndFiftyNine(3, 1);
+        FivehundardAndNineteen fivehundardAndFiftyNine = new FivehundardAndNineteen(3, 1);
         int [] res = fivehundardAndFiftyNine.flip();
         System.out.println(res);
     }
     int m, n, size;
     HashMap<Integer, Integer> map;
     private static final Random RANDOM = new Random();
-    public FivehundardAndFiftyNine(int m, int n) {
+    public FivehundardAndNineteen(int m, int n) {
         this.m = m;
         this.n = n;
         this.size = m *n;
