149f325ebe8d941b7339650e8b527c25f1548903
Chanter-Machine <9924000463@qq.com>
2021-12-12 15:27:22 +0800

    add 124;

--- src/main/java/algorithms/leetcode/tree/OnehundardAndTwentyFour.java
+++ src/main/java/algorithms/leetcode/tree/OnehundardAndTwentyFour.java
@@ -1,49 +1,62 @@
 package algorithms.leetcode.tree;
 
 import algorithms.leetcode.common.TreeNode;
+import sun.reflect.generics.tree.Tree;
 
 import java.util.Map;
 
 public class OnehundardAndTwentyFour {
     public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n1.right = n3;
 
+        int res = new OnehundardAndTwentyFour().maxPathSum(n1);
+        System.out.println(res);
     }
 
 
-    static int maxLeftPlusRight = 0;
+    static int maxLeftPlusRight;
     public int maxPathSum(TreeNode root) {
-        maxLeftPlusRight = 0;
-        return calc(root);
+        maxLeftPlusRight = Integer.MIN_VALUE;
+        calc(root);
+        return maxLeftPlusRight;
     }
 
     public int calc(TreeNode root) {
-
         if(root == null) {
             return 0;
         }
-
-        if (root.left == null && root.right == null) {
-            if (root.val < 0) {
-                return 0;
+        int res = root.val;
+        if(root.left == null && root.right ==  null) {
+            maxLeftPlusRight = Math.max(res, maxLeftPlusRight);
+        }else if(root.left != null && root.right !=  null){
+            int tempres = 0;
+            int maxLeftLen = calc(root.left);
+            int maxRightLen = calc(root.right);
+            if(maxLeftLen>0 ) {
+                tempres += maxLeftLen;
             }
-            return root.val;
+            if(maxRightLen > 0 ) {
+                tempres += maxRightLen;
+            }
+            maxLeftPlusRight = Math.max(tempres+res, maxLeftPlusRight);
+            int maxBranch = Math.max(maxLeftLen, maxRightLen);
+            if(maxBranch >0) {
+                res += maxBranch;
+            }
+        }else if (root.left != null ) {
+            int maxLeftLen = calc(root.left);
+            res = Math.max(root.val+ maxLeftLen, root.val);
+            maxLeftPlusRight = Math.max(res, maxLeftPlusRight);
+        }else {
+            int maxRightLen = calc(root.right);
+            res = Math.max(root.val+ maxRightLen, root.val);
+            maxLeftPlusRight = Math.max(res, maxLeftPlusRight);
         }
+        return res;
 
-        int leftLength = 0;
-        if (root.left != null) {
-            leftLength = maxPathSum(root.left);
-        }
-
-        int rightLength = 0;
-        if (root.right != null) {
-            rightLength = maxPathSum(root.right);
-        }
-
-        if(rightLength + leftLength+ root.val > maxLeftPlusRight) {
-            maxLeftPlusRight = leftLength + rightLength;
-        }
-        int maxLeftRight = Math.max(leftLength, rightLength);
-
-        return (maxLeftRight + root.val) >0?maxLeftRight:0;
     }
 }
