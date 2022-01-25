79d548a31cc85c185be6870431c33dea7271ef6d
Chanter-Machine <9924000463@qq.com>
2022-01-25 19:56:53 +0800

    updated 283;

--- src/main/java/algorithms/leetcode/array/TwohundardAndEightyThree.java
+++ src/main/java/algorithms/leetcode/array/TwohundardAndEightyThree.java
@@ -50,4 +50,17 @@ public class TwohundardAndEightyThree {
         arr[left] = arr[right];
         arr[right] = temp;
     }
+
+    public void moveZeroes3(int[] nums) {
+        int slow = 0;
+        for(int fast=0; fast<nums.length; fast++) {
+            if(nums[fast] != 0) {
+                nums[slow++] = nums[fast];
+            }
+        }
+        while (slow< nums.length) {
+            nums[slow++] = 0;
+        }
+
+    }
 }
