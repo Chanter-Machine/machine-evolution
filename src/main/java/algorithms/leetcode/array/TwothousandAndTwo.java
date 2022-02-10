0db579ad72af42bbc04a5c1d3a1acace3537badb
gaochaofan <gaochaofan@didiglobal.com>
2022-02-10 15:39:36 +0800

    updated 2002;

--- src/main/java/algorithms/leetcode/array/TwothousandAndTwo.java
+++ src/main/java/algorithms/leetcode/array/TwothousandAndTwo.java
@@ -41,4 +41,14 @@ public class TwothousandAndTwo {
         }
         return count;
     }
+
+    public int countKDifference2(int[] nums, int k) {
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            res += map.getOrDefault(nums[i]-k, 0) + map.getOrDefault(nums[i]+k, 0);
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        return res;
+    }
 }
