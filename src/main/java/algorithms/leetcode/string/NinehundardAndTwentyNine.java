b28d8dc9ddd3596ac4954e501a7764d2326695ea
Chanter-Machine <992400463@qq.com>
2022-06-04 10:43:02 +0800

    added 929; Unique Email Addresses 这题目贼jb无聊

---
+++ src/main/java/algorithms/leetcode/string/NinehundardAndTwentyNine.java
@@ -0,0 +1,17 @@
+package algorithms.leetcode.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NinehundardAndTwentyNine {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> emailSet = new HashSet<String>();
+        for (String email : emails) {
+            int i = email.indexOf('@');
+            String local = email.substring(0, i).split("\\+")[0]; // 去掉本地名第一个加号之后的部分
+            local = local.replace(".", ""); // 去掉本地名中所有的句点
+            emailSet.add(local + email.substring(i));
+        }
+        return emailSet.size();
+    }
+}
